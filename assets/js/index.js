362bd0155360a9cbf475721eb1a90bb163102c48
lambret <aurelie.l-02@hotmail.fr>
2018-10-09 09:57:29 +0200

    exercice 4

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+function delect() {
+  var lastname = document.getElementById("lastname").value = ""; ;
+  var firstname = document.getElementById("firstname").value = "";
+  var city = document.getElementById("city").value = "";
+  return false;
+}
